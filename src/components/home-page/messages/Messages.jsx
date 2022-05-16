3ddc9e2b9a70c9996b24c740c45077f6142ec8c4
AlinaTaoRao <raotao77@outlook.com>
2022-05-16 17:02:13 +0200

    create usePostFetch to handle new msg&adapt form

--- src/components/home-page/messages/Messages.jsx
+++ src/components/home-page/messages/Messages.jsx
@@ -1,85 +1,53 @@
 import React from "react";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 import "./styles.css";
 import useFetch from "../../../my-hook/useFetch";
-import useValueRef from '../../../my-hook/useValueRef';
+import useValueRef from "../../../my-hook/useValueRef";
+import usePostFetch from "../../../my-hook/usePostFetch";
 import { baseUrl } from "../../../config";
 import { curData } from "../../../data";
 import { jwt } from "../../../config";
 
-
-
-/* way 2-try 2:  */
+/* way 3: useEffect */
 export default function Messages({ username }) {
   const [newMsg, setNewMsg] = useState("");
   const [msgLength, setMsgLength] = useState(0); // define msgLength state to fire useFetch when new msg is send.
   console.log("msgLength in Messages:", msgLength);
-  const latestLength = useValueRef(msgLength); 
+  const [hasNewMsg, setHasNewMsg] = useState(false);
 
-  useEffect(() => {
-    latestLength.current = msgLength; // update
-  })
-
-  // useEffect(() => {
-  //   setMsgLength(l => l+1);
-  // },[newMsg])
-
-
+  /* customize usePostFetch to handle new message*/
+  const msgUrl = `${baseUrl}/messages`;
+  const { postData, postError, postLoading } = usePostFetch(
+    msgUrl,
+    newMsg,
+    hasNewMsg
+  );
+  console.log(" post Messages:", postData);
 
   /* define current channel url, global var curData.curCH value from Homepage*/
   const url = curData.curCh
     ? `${baseUrl}/channels/${curData.curCh}?populate=messages`
-    : `${baseUrl}/channels/2?populate=messages`;
+    : `${baseUrl}/channels/1?populate=messages`;
   const { data, error, loading } = useFetch(url, msgLength);
   console.log("Messages in cur ch:", data);
   if (loading) return <p> Loading</p>;
   if (error) return <p> Oops, there is something wrong :(, {error.message}</p>;
 
-  /* post new msg & update msgLength state to fire useFetch() */
-  const handleNewMsg = (e) => {
-    e.preventDefault();
-    setMsgLength(l => l+1);
-
-    const sender = curData.curUser;
-    console.log("sender=", sender);
-    const chId = curData.curCh;
-    // const userId = 2;     // # todo, dynamically get user id.
-
-    const msgUrl = `${baseUrl}/messages`;
-    async function addMsg(sender, chId, userId) {
-      const body = {
-        data: {
-          // users_permissions_users: {
-          //   id: userId,
-          // },
-          sender: sender,
-          body: newMsg,
-          channel: {
-            id: chId,
-          },
-        },
-      };
-
-      const token = jwt;
-      const response = await fetch(msgUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(body),
-      });
-      console.log("run handleNewMsg");
-      return response.json();
-    }
-
-    // addMsg(sender, chId, userId);
-    addMsg(sender, chId);
-  };
-
   return data.data.attributes.messages.data.length !== 0 ? (
     <div className="messages-col">
+      <div className="post-info-container">
+        {postError && (
+          <div className="post-error">
+            <p className="post-error-general error">
+              Oops, there is something wrong :(
+            </p>
+            <p className="post-error-status error">{error.status}</p>
+            <p className="post-error-msg error">{error.message}</p>
+          </div>
+        )}
+      </div>
+
       <div className="messages">
         {data.data.attributes.messages.data.map((msg, index) => (
           <div key={index} className="message">
@@ -93,7 +61,13 @@ export default function Messages({ username }) {
         ))}
       </div>
 
-      <div className="create-message">
+      <form
+        className="create-message"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setHasNewMsg(!hasNewMsg);
+        }}
+      >
         <input
           type="text"
           className="input-message"
@@ -102,38 +76,155 @@ export default function Messages({ username }) {
           onChange={(e) => setNewMsg(e.target.value)}
           required
         />
-        <button
-          className="send-msg"
-          id="send-msg"
-          onClick={(e) => handleNewMsg(e)}
-        >
-          Send
-        </button>
-      </div>
+        <input type="submit" value="Send" className="send-msg" />
+      </form>
     </div>
   ) : (
     <div className="messages-col">
       <div className="messages">{`"${data.data.attributes.title}" don't have any message yet.`}</div>
-      <div className="create-message">
+      <form
+        className="create-message"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setHasNewMsg(!hasNewMsg);
+        }}
+      >
         <input
           type="text"
           className="input-message"
+          placeholder="New message"
           value={newMsg}
           onChange={(e) => setNewMsg(e.target.value)}
-          placeholder="New message"
           required
         />
-        <button
-          className="send-msg"
-          id="send-msg"
-          onClick={(e) => handleNewMsg(e)}
-        >
-          Send
-        </button>
-      </div>
+        <input type="submit" value="Send" className="send-msg" />
+      </form>
     </div>
   );
 }
+
+/* way 2-try 2:  not work*/
+// export default function Messages({ username }) {
+//   const [newMsg, setNewMsg] = useState("");
+//   const [msgLength, setMsgLength] = useState(0); // define msgLength state to fire useFetch when new msg is send.
+//   console.log("msgLength in Messages:", msgLength);
+//   const latestLength = useValueRef(msgLength);
+
+//   useEffect(() => {
+//     latestLength.current = msgLength; // update
+//   })
+
+//   // useEffect(() => {
+//   //   setMsgLength(l => l+1);
+//   // },[newMsg])
+
+//   /* define current channel url, global var curData.curCH value from Homepage*/
+//   const url = curData.curCh
+//     ? `${baseUrl}/channels/${curData.curCh}?populate=messages`
+//     : `${baseUrl}/channels/2?populate=messages`;
+//   const { data, error, loading } = useFetch(url, msgLength);
+//   console.log("Messages in cur ch:", data);
+//   if (loading) return <p> Loading</p>;
+//   if (error) return <p> Oops, there is something wrong :(, {error.message}</p>;
+
+//   /* post new msg & update msgLength state to fire useFetch() */
+//   const handleNewMsg = (e) => {
+//     e.preventDefault();
+//     setMsgLength(l => l+1);
+
+//     const sender = curData.curUser;
+//     console.log("sender=", sender);
+//     const chId = curData.curCh;
+//     // const userId = 2;     // # todo, dynamically get user id.
+
+//     const msgUrl = `${baseUrl}/messages`;
+//     async function addMsg(sender, chId, userId) {
+//       const body = {
+//         data: {
+//           // users_permissions_users: {
+//           //   id: userId,
+//           // },
+//           sender: sender,
+//           body: newMsg,
+//           channel: {
+//             id: chId,
+//           },
+//         },
+//       };
+
+//       const token = jwt;
+//       const response = await fetch(msgUrl, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `Bearer ${token}`,
+//         },
+//         body: JSON.stringify(body),
+//       });
+//       console.log("run handleNewMsg");
+//       return response.json();
+//     }
+
+//     // addMsg(sender, chId, userId);
+//     addMsg(sender, chId);
+//   };
+
+//   return data.data.attributes.messages.data.length !== 0 ? (
+//     <div className="messages-col">
+//       <div className="messages">
+//         {data.data.attributes.messages.data.map((msg, index) => (
+//           <div key={index} className="message">
+//             <span className="sender">{msg.attributes.sender}</span>
+//             <span className="time">{msg.attributes.publishedAt}</span>
+//             <span className="ch-title">{data.data.attributes.title}</span>
+//             <p className="single-msg" data-msg-id={msg.id}>
+//               {msg.attributes.body}
+//             </p>
+//           </div>
+//         ))}
+//       </div>
+
+//       <div className="create-message">
+//         <input
+//           type="text"
+//           className="input-message"
+//           placeholder="New message"
+//           value={newMsg}
+//           onChange={(e) => setNewMsg(e.target.value)}
+//           required
+//         />
+//         <button
+//           className="send-msg"
+//           id="send-msg"
+//           onClick={(e) => handleNewMsg(e)}
+//         >
+//           Send
+//         </button>
+//       </div>
+//     </div>
+//   ) : (
+//     <div className="messages-col">
+//       <div className="messages">{`"${data.data.attributes.title}" don't have any message yet.`}</div>
+//       <div className="create-message">
+//         <input
+//           type="text"
+//           className="input-message"
+//           value={newMsg}
+//           onChange={(e) => setNewMsg(e.target.value)}
+//           placeholder="New message"
+//           required
+//         />
+//         <button
+//           className="send-msg"
+//           id="send-msg"
+//           onClick={(e) => handleNewMsg(e)}
+//         >
+//           Send
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
 
 /* way 2: get and render messages in a channel, works*/
 // export default function Messages({ username }) {
@@ -168,8 +259,6 @@ export default function Messages({ username }) {
 
 //     // setHasNewMsg(!hasNewMsg);      //not work
 //     // console.log("hasNewMsg update:", hasNewMsg);
-  
-  
 
 //     const sender = curData.curUser;
 //     console.log("sender=", sender);
---
+++ src/my-hook/usePostFetch.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { curData } from "../data";
+import { jwt } from "../config";
+
+const usePostFetch = (msgUrl, newMsg, hasNewMsg) => {
+  // const state
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // const [rerender, setRerender] = useState(false);
+
+  // define fetch data function
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        /* grab cur values and define body */
+        const sender = curData.curUser;
+        console.log("sender=", sender);
+        const chId = curData.curCh;
+        // const userId = 2;     // # todo, dynamically get user id.
+        const body = {
+          data: {
+            // users_permissions_users: {
+            //   id: userId,
+            // },
+            sender: sender,
+            body: newMsg,
+            channel: {
+              id: chId,
+            },
+          },
+        };
+
+        const token = jwt;
+
+        if (!newMsg) return;
+        const res = await fetch(encodeURI(msgUrl), {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(body),
+        });
+        console.log("res:", res);
+
+        // --- throw an error if the res is not ok (this works!) ---
+        if (!res.ok) {
+          const message = res.statusText
+            ? `${res.status}: ${res.statusText}\n-> ${msgUrl}`
+            : `HTTP error! status: ${res.status}\n-> ${msgUrl}`;
+          throw new Error(message);
+        }
+
+        const json = await res.json();
+        console.log("json:", json);
+
+        setData(json);
+        console.log("post data:", data);
+        setLoading(false);
+        // return json;
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [msgUrl, hasNewMsg]);
+
+  return { data, error, loading};
+};
+
+export default usePostFetch;
