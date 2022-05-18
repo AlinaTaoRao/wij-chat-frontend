be33b03a1a262b4f59facc53fb0dace833d19c38
AlinaTaoRao <raotao77@outlook.com>
2022-05-18 10:44:27 +0200

    edit data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
 export const curData = {
     // curCh:1,
     // curUser:null,
-    curUserId:null
+    curUserId:null,  // will assign value when usr sign in, from responst.json();
+    jwtToken:null,  // will assign value when usr sign in; from responst.json();
 };
 
 export const usrPostData={};
